3384b5da4a5e5d2537d1428a540dfe5cdef710ad
Ildar Bikmamatov <ildar@bayrell.org>
2026-08-23 00:06:39 +0500

    add build npm

--- tools/build.sh
+++ tools/build.sh
@@ -6,8 +6,14 @@ BASE_PATH=`dirname $SCRIPT_PATH`
 case $1 in
 
     app)
-        docker run -it --rm -v $BASE_PATH:/app node:22-slim \
+        docker run -it --rm -v $BASE_PATH:/app \
+            --user node node:22-slim \
             bash -c "cd /app && npm run build && npm run build_plugin"
+    ;;
+    
+    npm)
+        docker run -it --rm -v $BASE_PATH:/app \
+            --user node node:22-slim npm ${@:2}
     ;;
     
     *)
